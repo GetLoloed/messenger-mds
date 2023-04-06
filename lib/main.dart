836a61bec0a51eb4fa5f84118e11f9bfc0e89360
lolo <loic.aviez91@gmail.com>
2023-04-06 09:23:01 +0200

    Ajout des routes /accueil /inscription et /connexion

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import './views/dashboard_view.dart';
+import 'views/accueil_view.dart';
+import 'views/connexion_view.dart';
+import 'views/inscription_view.dart'; // importez votre vue d'inscription ici
 
-void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Messenger'),
+      title: 'Messagerie',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => AccueilView(),
+        '/connexion': (context) => const ConnexionView(),
+        '/inscription': (context) => const InscriptionView(), // définir la route d'inscription
+      },
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(),
-      ),
-    );
-  }
-}
-
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Dashboard',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: DashboardView(),
-  );
 }
